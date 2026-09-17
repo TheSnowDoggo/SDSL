cfc8f1cb1811a7f071cce9dadf3ee0b814c8af86
Luna Bl3p <redstonewolfer@Gmail.com>
2026-09-17 19:27:10 +0100

    small change

--- SDSL/src/assembly/AssemblyGenerator.cs
+++ SDSL/src/assembly/AssemblyGenerator.cs
@@ -28,7 +28,7 @@ public class AssemblyGenerator
 
 	private void GenerateFunctions(UserClass userClass)
 	{
-		foreach (Function function in userClass.LocalUserFunctions)
+		foreach (UserFunction function in userClass.LocalUserFunctions)
 		{
 			new FunctionParser(_assembly, (UserFunction)function).Parse();
 		}
@@ -69,7 +69,7 @@ public class AssemblyGenerator
 	
 	private void GenerateProperties(UserClass userClass)
 	{
-		foreach (Property property in userClass.LocalUserProperties)
+		foreach (UserProperty property in userClass.LocalUserProperties)
 		{
 			var userProperty = (UserProperty)property;
 
